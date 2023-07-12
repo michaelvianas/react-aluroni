bca84da6636ffc8bca508ea8db9419be93658746
michaelvianas <vmichael116@gmail.com>
2023-07-12 11:23:58 -0300

    Iniciando com as rotas

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,20 +1,18 @@
-import styles from "./Cardapio.module.scss";
-import { ReactComponent as Logo } from "assets/logo.svg";
-import Buscador from "./Buscador";
-import { useState } from "react";
-import Filtros from "./Filtros";
-import Ordenador from "./Ordenador";
+import styles from './Cardapio.module.scss';
+import Buscador from './Buscador';
+import { useState } from 'react';
+import Filtros from './Filtros';
+import Ordenador from './Ordenador';
 import Itens from './Itens';
+import React from 'react';
+import Menu from 'components/menu';
 
 export default function Cardapio() {
-  const [busca, setBusca] = useState("");
+  const [busca, setBusca] = useState('');
   const [filtro, setFiltro] = useState<number | null>(null);
-  const [ordenador, setOrdenador] = useState("");
+  const [ordenador, setOrdenador] = useState('');
   return (
     <main>
-      <nav className={styles.menu}>
-        <Logo />
-      </nav>
       <header className={styles.header}>
         <div className={styles.header__text}>
           A casa do código e da massa
@@ -30,5 +28,5 @@ export default function Cardapio() {
         <Itens busca={busca} filtro={filtro} ordenador={ordenador} />
       </section>
     </main>
-  )
+  );
 }
