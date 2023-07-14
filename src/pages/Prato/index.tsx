6415bfc1fdc50142a10d8a4974352133e074e04a
michaelvianas <vmichael116@gmail.com>
2023-07-14 11:21:56 -0300

    começando com useLocation

---
+++ src/pages/Prato/index.tsx
@@ -0,0 +1,10 @@
+import styles from './Prato.module.scss';
+import { useLocation } from 'react-router-dom';
+
+export default function Prato() {
+  return (
+    <div className={styles}>
+      Prato
+    </div>
+  );
+}
